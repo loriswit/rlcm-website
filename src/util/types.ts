5b581a00a059e582bb7f93a6e656388cd024c3f1
Loris Witschard <loris.wit@gmail.com>
2024-08-27 12:12:37 +0200

    feat: add changelog

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,13 +1,27 @@
 export interface GitHubRelease {
-    tag_name: string,
+    tag_name: string
     assets: GithubReleaseAsset[]
+    published_at: string
+    body: string
     message?: string
 }
 
 export interface GithubReleaseAsset {
-    name: string,
-    download_count: number,
-    browser_download_url: string,
+    name: string
+    download_count: number
+    browser_download_url: string
+}
+
+export interface VersionInfo {
+    number: number
+    name: string
+    url: string
+}
+
+export interface ChangelogEntry {
+    title: string
+    date: Date
+    changes: string[]
 }
 
 export interface CountryInfo {
